75ae4014213492152740d9f845faa4c442fe3c75
MiquelFerreiro <miquel.ferreiro01@estudiant.upf.edu>
2025-12-01 09:46:32 +0100

    hasta crop

--- run_pos.py
+++ run_pos.py
@@ -4,7 +4,8 @@ from POS_WANG import POS_WANG
 import utils
 
 # Video a procesar
-video_path = "vid.avi"
+#video_path = "vid.avi"
+video_path = "vid_crop.avi"
 
 # Cargar video y convertir a RGB
 cap = cv2.VideoCapture(video_path)
@@ -13,7 +14,7 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-    frame = cv2.resize(frame, (128, 128))  # resize opcional
+    #frame = cv2.resize(frame, (128, 128))  # resize opcional
     frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 cap.release()
 
